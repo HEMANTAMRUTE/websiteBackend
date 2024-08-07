d24eaaa54b99da36a06d50edc01ec9f68a535435
unknown <hemantamrute12345@gmail.com>
2024-08-07 17:12:34 +0530

    Fix middleware usage and export routes correctly

--- Routes/index.js
+++ Routes/index.js
@@ -5,7 +5,7 @@ const intern=require("./internshipRout")
 const job=require("./jobRoute")
 const admin=require("./admin")
 const otp=require("./server")
-const smsRouter = require("./Sms");
+// const smsRouter = require("./Sms");
 const Login =require("./loginController");
 router.get("/",(req,res)=>{
     res.send("the is backend")
@@ -15,6 +15,6 @@ router.use('/internship',intern);
 router.use('/job',job);
 router.use('/admin',admin);
 router.use('/otp',otp)
-router.use('/sms', smsRouter);
+// router.use('/sms', smsRouter);
 router.use('/Login',Login);
 module.exports=router;
